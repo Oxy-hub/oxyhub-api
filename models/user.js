1e211042dae0136b9599684a4307861c2c200ce8
saby22 <sabyasachi.tffs@gmail.com>
2021-08-20 14:06:30 +0530

    Added prettier

--- models/user.js
+++ models/user.js
@@ -1,29 +1,29 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
-	email: {
-		type: String,
-		required: true,
-	},
-	first_name: {
-		type: String,
-		uppercase: true,
-		trim: true,
-	},
-	middle_name: {
-		type: String,
-		uppercase: true,
-		trim: true,
-	},
-	last_name: {
-		type: String,
-		uppercase: true,
-		trim: true,
-	},
-	isInitial: {
-		type: Boolean,
-		default: true,
-	},
+  email: {
+    type: String,
+    required: true
+  },
+  first_name: {
+    type: String,
+    uppercase: true,
+    trim: true
+  },
+  middle_name: {
+    type: String,
+    uppercase: true,
+    trim: true
+  },
+  last_name: {
+    type: String,
+    uppercase: true,
+    trim: true
+  },
+  isInitial: {
+    type: Boolean,
+    default: true
+  }
 });
 
 const User = mongoose.model('User', userSchema);
