bef99aa76e2b4920fed82cfac1b092eff64f381d
saby22 <sabyasachi.tffs@gmail.com>
2022-02-28 23:16:25 +0530

    Updated controller for districts

--- src/controllers/districts/fetchDistricts.js
+++ src/controllers/districts/fetchDistricts.js
@@ -1,8 +1,17 @@
-// const { Container } = require('../../loaders/awilix');
-// const config = require('../../config');
-// const { createSuccessDto, authResponseDto } = require('../../dto');
+const { Container } = require('../../loaders/awilix');
+const { createSuccessDto, districts: districtDtos } = require('../../dto');
 
 exports.fetchDistricts = async (req, res) => {
-  console.log('Query Districts', req.query);
-  res.send('OK');
+  const { state } = req.query;
+
+  const DistrictService = Container.resolve('districtService');
+
+  const districts = await DistrictService.fetchDistricts(state);
+
+  return res.send(
+    createSuccessDto(
+      `Districts for the state ${state} successfully found!`,
+      districtDtos.getResponse(districts)
+    )
+  );
 };
