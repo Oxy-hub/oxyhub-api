0c28564b873abb186632256901c4dbb24f7ed532
saby22 <sabyasachi.tffs@gmail.com>
2022-02-28 23:14:17 +0530

    Registered district service and repository in awilix and cleaned up the file

--- src/loaders/awilix.js
+++ src/loaders/awilix.js
@@ -1,8 +1,15 @@
 const awilix = require('awilix');
+// Service imports
 const { UserService } = require('../services/UserService');
 const { TokenService } = require('../services/TokenService');
+const { DistrictService } = require('../services/DistrictService');
+
+// Repository imports
 const { UserRepository } = require('../repositories/UserRepository');
 const { TokenRepository } = require('../repositories/TokenRepository');
+const { DistrictRepository } = require('../repositories/DistrictRepository');
+
+// Other imports
 const GithubRepository = require('../repositories/GithubRepository');
 const MongooseUserModel = require('../models/User');
 
@@ -13,11 +20,18 @@ const Container = awilix.createContainer({
 
 const awilixInit = ({ redisClient: redis }) => {
   Container.register({
+    // Services go here
     userService: awilix.asClass(UserService),
     tokenService: awilix.asClass(TokenService),
+    districtService: awilix.asClass(DistrictService),
+
+    // Repositories go here
     userRepository: awilix.asClass(UserRepository),
     tokenRepository: awilix.asClass(TokenRepository),
     githubRepository: awilix.asClass(GithubRepository),
+    districtRepository: awilix.asClass(DistrictRepository),
+
+    // Other stuff goes here
     mongooseUserModel: awilix.asValue(MongooseUserModel),
     redisClient: awilix.asValue(redis)
   });
