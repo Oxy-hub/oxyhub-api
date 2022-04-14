695edd38e6437735ffa8465e2858dca1254da4df
saby22 <sabyasachi.tffs@gmail.com>
2022-04-15 01:37:07 +0530

    Refactored service, repository and controller for /refresh endpoint

--- src/controllers/refresh/refreshUser.js
+++ src/controllers/refresh/refreshUser.js
@@ -1,59 +1,20 @@
-// const tokenVerifier = require('../../utils/helpers/tokenVerifierHelper');
-// const {
-//   findRefreshToken,
-//   deleteRefreshToken,
-//   findIsInitial
-// } = require('../../utils/helpers/redisHelpers');
-
-// const wrapAsync = require('../wrapAsync');
-// const AppError = require('../../utils/AppError');
-
-// exports.validateRefreshToken = wrapAsync(async (req, res, next) => {
-//   const { RTK } = req.cookies;
-//   // Try to verify the refresh token's validity
-//   const { jti, id } = tokenVerifier(RTK, 'REFRESH');
-
-//   // Check for the userid:jti key in redis, will return true if present
-//   const response = await findRefreshToken(id, jti);
-//   if (!response) throw new AppError('Invalid refresh token', 400);
-
-//   // Deleting the corresponding userid:jti key in redis
-//   await deleteRefreshToken(id, jti);
-
-//   // Try to find if user is set as an initial user in redis
-//   req.isInitial = await findIsInitial(id);
-
-//   // Attach the user id to the req object for the generate token middleware
-//   req.user_id = id;
-//   console.log('Successfully validated the Refresh Token');
-//   next();
-// });
-
 const { Container } = require('../../loaders/awilix');
-const { createSuccessDto, refreshResponseDto } = require('../../dto');
+const { createSuccessDto, refresh: refreshDto } = require('../../dto');
 const config = require('../../config');
 
-exports.validateRefreshToken = async (req, res) => {
-  // Read RTK (refresh_token from the cookies)
-  console.log('1');
+exports.refreshUser = async (req, res) => {
+  // Extract refresh token from cookies
   const { RTK: userRefreshToken } = req.cookies;
-  console.log('Refresh Token', userRefreshToken);
 
-  console.log('A');
-  // Resolve token service from container
-  const TokenService = Container.resolve('tokenService');
+  // Resolve auth service from container
+  const AuthService = Container.resolve('authService');
 
-  console.log('B');
-  // Fetch the User from user service
-  const userId = await TokenService.verifyRefreshToken(userRefreshToken);
-  // const userId = '223abc';
+  // Fetch the userId from the refresh token
+  const userId = await AuthService.verifyRefreshToken(userRefreshToken);
 
-  console.log('C');
-  const refreshToken = await TokenService.generateRefreshToken(userId);
-  console.log(refreshToken);
-
-  console.log('D');
-  const accessToken = TokenService.generateAccessToken(userId);
+  // Generate new pair of access and refresh tokens
+  const accessToken = AuthService.generateAccessToken(userId);
+  const refreshToken = await AuthService.generateRefreshToken(userId);
 
   return res
     .cookie('RTK', refreshToken, {
@@ -64,7 +25,7 @@ exports.validateRefreshToken = async (req, res) => {
     .send(
       createSuccessDto(
         'User logged in successfully!',
-        refreshResponseDto({
+        refreshDto.getResponse({
           accessToken
         })
       )
