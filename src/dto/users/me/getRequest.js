bd0d459b38ca08e2acfc42b3334f60e74ae09d60
saby22 <sabyasachi.tffs@gmail.com>
2022-04-17 21:03:14 +0530

    Removed user id from the response DTO which fetches logged in user's details

--- src/dto/users/me/getRequest.js
+++ src/dto/users/me/getRequest.js
@@ -1,10 +1,8 @@
 module.exports = res => ({
-  id: res.id,
   first_name: res.firstName,
   middle_name: res.middleName,
   last_name: res.lastName,
   email: res.email,
   avatar: res.avatar,
-  created_at: res.createdAt,
-  last_login: res.lastLogin
+  created_at: res.createdAt
 });
