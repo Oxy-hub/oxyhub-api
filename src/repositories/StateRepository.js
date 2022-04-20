65074ab9a9610b6f8a4ef3a4f29b64263e08c272
saby22 <sabyasachi.tffs@gmail.com>
2022-04-20 19:37:07 +0530

    Introduced readState method and modified the name of the variable used to store all states

--- src/repositories/StateRepository.js
+++ src/repositories/StateRepository.js
@@ -3,12 +3,17 @@ class StateRepository {
     this.StateModel = StateModel;
   }
 
+  async readState(filter) {
+    const state = await this.StateModel.findOne(filter).exec();
+    return state;
+  }
+
   async readAllStates() {
-    const res = await this.StateModel.find({})
+    const states = await this.StateModel.find({})
       .sort({ name: 1 })
       .select({ name: 1, code: 1 })
       .exec();
-    return res;
+    return states;
   }
 }
 
