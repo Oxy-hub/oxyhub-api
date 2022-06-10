5e625c53094f5266ed28e488e1df24802c3f0fb9
saby22 <sabyasachi.tffs@gmail.com>
2022-06-11 01:13:33 +0530

    Updated GET /parlours response DTO to include owner name

--- src/dto/parlours/response.get.js
+++ src/dto/parlours/response.get.js
@@ -5,10 +5,12 @@ module.exports = parlours => ({
           ({
             name,
             storeId,
+            ownerName,
             location: { stateCode, district, address, pincode }
           }) => ({
             name,
             store_id: storeId,
+            owner_name: ownerName,
             location: { state_code: stateCode, district, address, pincode }
           })
         )
