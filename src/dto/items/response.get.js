1b04c060fbaa22d021b260e37c9537c2bb037dc0
saby22 <sabyasachi.tffs@gmail.com>
2022-06-11 19:32:11 +0530

    Updated response DTO for GET /items

--- src/dto/items/response.get.js
+++ src/dto/items/response.get.js
@@ -1,1 +1,16 @@
-module.exports = res => res;
+module.exports = items => ({
+  items: items.map(({ storeId, variants, productId, cylinderType }) => ({
+    store_id: storeId,
+    product_id: productId,
+    cylinder_type: cylinderType,
+    variants: variants.map(
+      ({ name, height, price, waterCapacity, oxygenCapacity }) => ({
+        name,
+        height,
+        waterCapacity,
+        oxygenCapacity,
+        price
+      })
+    )
+  }))
+});
